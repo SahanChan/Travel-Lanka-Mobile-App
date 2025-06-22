c698eedc079904310b4cc8d9df75244c6d9c8f21
SahanChan <xnscb8.chandrabahu@gmail.com>
2025-06-22 18:34:22 +1000

    added navbar pages

--- Frontend/lanka_journey/app/index.tsx
+++ Frontend/lanka_journey/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome to Lanka Journey</Text>
     </View>
   );
 }
